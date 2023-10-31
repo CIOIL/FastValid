52399d43cc75f3a4c3e1f6924a8237e2aa3d0419
YaakovHatam <6986221+YaakovHatam@users.noreply.github.com>
2023-10-31 13:56:41 +0200

    headless test

--- projects/fast-valid-demo-app/src/app/app.component.spec.ts
+++ projects/fast-valid-demo-app/src/app/app.component.spec.ts
@@ -12,16 +12,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fast-valid-demo-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('fast-valid-demo-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fast-valid-demo-app app is running!');
-  });
 });
